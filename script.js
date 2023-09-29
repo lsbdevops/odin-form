3c61f588e6814742bbb6ef9810f7de3b49568abc
lsbdevops <lsbdevops@gmail.com>
2023-09-29 16:43:14 +1000

    Add validation for password and confirmation
    Upon form submission, script now checks if the password and
    password confirmation match, otherwise submission is prevented
    and error class is applied to password and confirmation fields.

--- script.js
+++ script.js
@@ -9,7 +9,18 @@ signupForm.addEventListener("submit", validateForm);
 function validateForm(e) {
     const form = e.target;
     
-    if (!form.checkValidity()) {
+    if (form.checkValidity()) {
+        // Confirm that the password and confirmation match.
+        const password = document.querySelector("#password");
+        const passwordConfirmation = document.querySelector("#confirmation");
+
+        if (password.value !== passwordConfirmation.value) {
+            e.preventDefault();
+            password.classList.add("error");
+            passwordConfirmation.classList.add("error");
+        }
+    }
+    else {
         // If form isn't valid, prevent submission.
         e.preventDefault();
 
