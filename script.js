04af11cf0cf94e2786621bc3ee0d959755ae0792
lsbdevops <lsbdevops@gmail.com>
2023-09-30 19:31:55 +1000

    Add custom validation messages for all inputs
    Validation error/help messages now displayed directly below the
    input boxes in red text. Custom messages have been defined to
    provide better feedback to the user on what is required to
    complete correct entry for the specific field type.

--- script.js
+++ script.js
@@ -10,6 +10,17 @@ function validateForm(e) {
     const form = e.target;
     
     if (form.checkValidity()) {
+        // Remove all outstanding error messages.
+        for (const element of form.elements) {
+            if (element.nodeName !== "INPUT") {
+                continue;
+            }
+            
+            element.classList.remove("error");
+            element.closest("ul").querySelector(".help").classList.remove("error");
+            element.closest(".form-row").classList.remove("error");
+        }
+
         // Confirm that the password and confirmation match.
         const password = document.querySelector("#password");
         const passwordConfirmation = document.querySelector("#confirmation");
@@ -18,6 +29,10 @@ function validateForm(e) {
             e.preventDefault();
             password.classList.add("error");
             passwordConfirmation.classList.add("error");
+
+            const help = password.closest("ul").querySelector(".help");
+            help.classList.add("error");
+            help.textContent = "Passwords do not match.";
         }
     }
     else {
@@ -26,11 +41,51 @@ function validateForm(e) {
 
         // For all invalid form elements, set error class for appropriate styling.
         for (const element of form.elements) {
+
+            // If element is not an input element, proceed to the next element in the node list.
+            if (element.nodeName !== "INPUT") {
+                continue;
+            }
+
+            // Remove/add error class based on field validity.
             if (element.checkValidity()) {
                 element.classList.remove("error");
+                element.closest("ul").querySelector(".help").classList.remove("error");
+                
+                // Only remove error class from row div element if the first input element is also valid.
+                if (element.closest(".form-row").querySelector("input").checkValidity()) {
+                    element.closest(".form-row").classList.remove("error");
+                }
             }
             else {
                 element.classList.add("error");
+                element.closest(".form-row").classList.add("error");
+
+                // Update class and text content for the associated 'help' element.
+                const help = element.closest("ul").querySelector(".help");
+                help.classList.add("error");
+
+                if (element.validity.valueMissing) {
+                    help.textContent = "Input is required for this field";
+                }
+                else if (element.validity.patternMismatch) {
+                    switch(element.type) {
+                        case "text":
+                            help.textContent = "Name must be letters only";
+                            break;
+                        case "tel":
+                            help.textContent = "Phone must be numbers only";
+                            break;
+                    }
+                }
+                else if (element.validity.typeMismatch)
+                {
+                    help.textContent = "Email must be in format of you@example"
+                }
+                // Otherwise, default to the standard validation message.
+                else {
+                    help.textContent = element.validationMessage;
+                }
             }
         }
     }
